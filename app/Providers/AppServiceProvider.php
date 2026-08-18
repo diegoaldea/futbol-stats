85076ee066b7d2f1a8aaafcad5191da798ce7979
diegoaldea <diegolucianoaldea@gmail.com>
2026-08-18 01:25:02 -0300

    chore: force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+         if (env('APP_ENV') !== 'local') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
